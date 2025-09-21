01b11947f2b4ac25a8e98dab8dffd32a824d6055
Amin Zamani Afshar <zamani35099@gmail.com>
2025-09-21 19:06:02 +0330

    add Initial setup and Database config

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
+using ECommerce.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.AddAppDbContext();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.Services.MigrateDatabase();
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-
-
 app.Run();
 
